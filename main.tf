85e2ceea7445267ef48958a3d6655d1a04304205
John Webb <johwebb1@publicisgroupe.net>
2022-11-07 14:20:16 -0600

    working on main

--- main.tf
+++ main.tf
@@ -1,9 +1,32 @@
-locals {
-  environment = "${lookup(var.workspace_to_env_map, terraform.workspace, "staging")}"
-  project_id = "${var.env_to_project_id_map[local.environment]}"
+resource "google_sql_database" "sql_database" {
+  name = "my-database"
+  instance = google_sql_database_instance.sql_database_instance.name
 }
 
-module "instances" {
+resource "google_sql_database_instance" "sql_database_instance" {
+  name = "my-database-instance"
+  database_version = "MYSQL_5_7"
+  region = var.region
+
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+resource "google_sql_user" "sql_user" {
+  name = "mysql_user"
+  instance = google_sql_database_instance.sql_database_instance.name
+  host = 
+  password = 
+}
+
+resource "random_password" "pwd" {
+  length = 16
+  special = false
+}
+
+
+module "sql_database" {
   # Must be hard coded
   source  = "app.terraform.io/jrw-test-org/instances/google"
   version = "v1.0.12"
@@ -14,4 +37,22 @@ module "instances" {
   machine_type = "${var.compute_size_map[local.environment]}"
   allow_stopping_for_update = var.allow_stopping_map[local.environment]
   zone = var.zone_env_map[local.environment]
+}
+
+
+
+
+resource "azurerm_resource_group" "vnet" {
+  name     = var.resource_group_name
+  location = "East US"
+}
+
+module "vnet" {
+  source              = "Azure/vnet/azurerm"
+  resource_group_name = azurerm_resource_group.vnet.name
+  address_space       = ["10.0.0.0/16"]
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+
+  depends_on = [azurerm_resource_group.vnet]
 }
