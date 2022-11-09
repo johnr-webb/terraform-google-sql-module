99362480d476f71851a87f5b80d9c4eb1a2ee54c
John Webb <johwebb1@publicisgroupe.net>
2022-11-09 14:47:38 -0600

    comment out password output

--- outputs.tf
+++ outputs.tf
@@ -7,9 +7,10 @@ output "database_username" {
   description = "Username for SQL user"
   value = google_sql_user.sql_user.name
 }
-
+/*
 output "database_password" {
   sensitive = true
   description = "Password for SQL user"
   value = google_sql_user.sql_user.password
 }
+*/
