6f3d10f956d7f77f7db56f430d7d3fd5ea633e3e
johnr-webb <117673293+johnr-webb@users.noreply.github.com>
2022-11-14 13:02:34 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "database_host" {
   value = google_sql_database.sql_database.self_link
 }
 
+output "database_connection_name" {
+  description = "Connection name for SQL instance"
+  value = google_sql_database_instance.sql_database_instance.connection_name
+}
+
 output "database_username" {
   description = "Username for SQL user"
   value = google_sql_user.sql_user.name
